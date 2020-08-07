6c372dac965ca3778319cd65f6db2fd86a45774a
cjogles <jackson.ogles28@gmail.com>
2020-08-07 09:15:13 -0600

    finished the ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,11 +1,21 @@
-def has_negatives(a):
+def has_negatives(the_input):
     """
     YOUR CODE HERE
     """
     # Your code here
-
+    cache = {}
+    result = []
+    for i in the_input:
+        if i > 0:
+            cache[i] = 0
+        else:
+            continue
+    for i in the_input:
+        if i < 0:
+            i = -i 
+            if i in cache:
+                result.append(i)
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
